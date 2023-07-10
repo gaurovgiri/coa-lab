e8bf99b814ee29b862e629696ec0fd0f13ad6c75
gaurovgiri <gaurovgiri@gmail.com>
2023-07-10 18:06:19 +0545

    add comment in lab3_array

--- lab3_array.cpp
+++ lab3_array.cpp
@@ -1,84 +1,81 @@
 #include <iostream>
 
 using namespace std;
-void BinaryMultiply(int *, int *, int *, int, int, int);
 
-int main()
-{
+/**
+ * The function `multiplyBinary` performs binary multiplication on two arrays of binary digits and
+ * stores the result in another array.
+ * 
+ * @param multiplicand An array representing the binary digits of the multiplicand. The size of the
+ * array is given by size1.
+ * @param size1 The parameter `size1` represents the size of the `multiplicand` array.
+ * @param multiplier The `multiplier` parameter is an array of integers representing a binary number.
+ * Each element in the array can be either 0 or 1.
+ * @param size2 The parameter `size2` represents the size of the `multiplier` array.
+ * @param result The `result` parameter is an array that will store the result of the binary
+ * multiplication.
+ */
+void multiplyBinary(int multiplicand[], int size1, int multiplier[], int size2, int result[]) {
+    int sizeResult = size1 + size2;
 
-    int *multplicand, *multiplier, *result;
-    int multiplicandSize, multiplierSize, resultSize;
-
-    cout << "Enter the size of the Multiplicand: ";
-    cin >> multiplicandSize;
-
-    cout << "Enter the size of the Multiplier: ";
-    cin >> multiplierSize;
-
-    resultSize = multiplicandSize + multiplierSize;
-
-    multplicand = new int[multiplicandSize];
-    multiplier = new int[multiplierSize];
-    result = new int[resultSize];
-
-    cout << "Enter the Multiplicand: ";
-    for (int i = 0; i < multiplicandSize; i++)
-    {
-        cin >> multplicand[i];
+    // Initialize the result array with zeros
+    for (int i = 0; i < sizeResult; ++i) {
+        result[i] = 0;
     }
 
-    cout << "Enter the Multiplier: ";
-    for (int i = 0; i < multiplierSize; i++)
-    {
+    // Perform binary multiplication
+    for (int i = size1 - 1; i >= 0; --i) {
+        for (int j = size2 - 1; j >= 0; --j) {
+            int product = multiplicand[i] * multiplier[j];
+
+            // Add the product to the result array
+            result[i + j + 1] += product;
+            result[i + j] += result[i + j + 1] / 2; // Add carry to next bit
+            result[i + j + 1] %= 2; // Remove carry from current bit
+        }
+    }
+}
+
+int main() {
+    int size1, size2;
+
+    // Read the size of the multiplicand
+cout << "Enter the size of the multiplicand: ";
+    cin >> size1;
+
+    // Read the size of the multiplier
+    cout << "Enter the size of the multiplier: ";
+    cin >> size2;
+
+    // Read the multiplicand
+    int multiplicand[size1];
+    cout << "Enter the multiplicand: ";
+    for (int i = 0; i < size1; ++i) {
+        cin >> multiplicand[i];
+    }
+
+    // Read the multiplier
+    int multiplier[size2];
+    cout << "Enter the multiplier: \t";
+    for (int i = 0; i < size2; ++i) {
         cin >> multiplier[i];
     }
 
-    BinaryMultiply(multplicand, multiplier, result, multiplicandSize, multiplierSize, resultSize);
+    // Calculate the size of the result array
+    int sizeResult = size1 + size2;
 
-    cout << "Result: ";
-    for (int i = 0; i < resultSize; i++)
-    {
-        cout << result[i];
+    // Initialize the result array
+    int result[sizeResult];
+
+    // Multiply the binary numbers
+    multiplyBinary(multiplicand, size1, multiplier, size2, result);
+
+    // Display the product array
+    cout << "Product array: ";
+    for (int i = 0; i < sizeResult; ++i) {
+        cout << result[i] << " ";
     }
     cout << endl;
 
     return 0;
-}
-
-/**
- * The BinaryMultiply function performs binary multiplication on two arrays of integers and stores the
- * result in another array.
- * 
- * @param multiplicand An array representing the binary digits of the multiplicand.
- * @param multiplier An array representing the binary representation of the multiplier. Each element of
- * the array can be either 0 or 1.
- * @param result The `result` parameter is an array that will store the result of the binary
- * multiplication.
- * @param multiplicandSize The size of the array `multiplicand`, which represents the number being
- * multiplied.
- * @param multiplierSize The parameter `multiplierSize` represents the size of the array `multiplier`.
- * It indicates the number of elements in the `multiplier` array.
- * @param resultSize The parameter `resultSize` represents the size of the `result` array. It indicates
- * the number of bits required to store the result of the binary multiplication.
- */
-void BinaryMultiply(int *multiplicand, int *multiplier, int *result, int multiplicandSize, int multiplierSize, int resultSize)
-{
-    int rawSum, carry = 0;
-    for (int i = 0; i < resultSize; i++)
-    {
-        result[i] = 0;
-    }
-
-    for (int i = multiplierSize - 1; i >= 0; i--)
-    {
-        if (multiplier[i] == 1)
-        {
-            for (int j = multiplicandSize - 1; j >= 0; j--)
-            {
-                rawSum = multiplicand[j] + result[i + j + 1] + carry;
-                result[i + j + 1] = rawSum % 2;
-                carry = rawSum / 2;
-            }
-        }
-    }
 }
